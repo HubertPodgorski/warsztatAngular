a153f3b89166a67559ea1292f26a5a4eb506dc5a
Hubert <hubert.podgorski@globegroup.pl>
2017-02-27 10:16:36 +0100

    tworzenie klasy produkt

--- src/app/produkt/produkt.component.ts
+++ src/app/produkt/produkt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produkt } from './produkt';
 
 @Component({
   selector: 'app-produkt',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProduktComponent implements OnInit {
 
+  Produkt: Produkt;
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
---
+++ src/app/produkt/produkt.ts
@@ -0,0 +1,20 @@
+export class Produkt {
+    // name: string;
+    // description?: string;
+    // id: number;
+    // category: string;
+
+    // constructor(id, name, category, description){
+    //     this.id = id;
+    //     this.name = name;
+    //     this.category = category;
+    //     this.description = description;
+    // }
+
+    constructor(
+        public id: number;
+        public name: string;
+        public category: string;
+        public description?: string;
+    ){}
+}
