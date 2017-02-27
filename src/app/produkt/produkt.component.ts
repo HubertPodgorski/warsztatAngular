296da74c00ecad0d066bfcc67276320e900824c3
Hubert <hubert.podgorski@globegroup.pl>
2017-02-27 11:01:55 +0100

    form reset

--- src/app/produkt/produkt.component.ts
+++ src/app/produkt/produkt.component.ts
@@ -10,8 +10,10 @@ export class ProduktComponent implements OnInit {
 
   Produkt: Produkt;
   customPlaceholder: string;
+  categories: string[];
 
   constructor() {
+    this.categories = ["kategoria1", "kategoria2", "trzecia kategoria", "siedemnasta kategoria", "nie trzecia kategoria", "AGD"];
     this.customPlaceholder = "nazwa";
    }
 
